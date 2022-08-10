221a274b32ddec16f5073359c58443b5322b4eba
Hardik5247 <thamihardik8@gmail.com>
2022-08-10 23:58:04 +0530

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+
+# india-landslide-gis [![Python 3.6](https://img.shields.io/badge/python-3.6+-blue.svg)](https://www.python.org/downloads/release/python-360/)
+
+## Setup
+```
+pip install --upgrade pip setuptools
+pip install -r requirements.txt
+```
+
+## Run
+```
+python main.py
+```
---
+++ main.py
@@ -0,0 +1,53 @@
+import requests
+import time
+import os
+
+from flask import Flask, jsonify
+from flask_cors import CORS, cross_origin
+
+from pandas import DataFrame
+from spacy import displacy, load
+from bs4 import BeautifulSoup
+
+app = Flask(__name__)
+CORS(app, support_credentials=True)
+
+nlp = load('en_core_web_trf')  # en_core_web_trf gives better results than en_core_web_sm
+
+
+@app.route('/<int:pages>', methods=['GET'])
+def start(pages):
+    data = []
+    baseUrl = 'https://timesofindia.indiatimes.com/topic/landslides/news/{}'
+
+    for page in range(1, pages + 1):
+        url = baseUrl.format(str(page))
+
+        try:
+            page = requests.get(url)
+            time.sleep(2)
+        except Exception as e:
+            print(e)
+            continue
+
+        soup = BeautifulSoup(page.text, 'html.parser')
+        links = soup.find_all('div', attrs={'class': 'Mc7GB'})
+        data.extend(links)
+
+    for i in range(len(data)):
+        news = data[i]
+
+        title = news.find("span").text.strip()
+        body = news.find("p").find("span").text.strip()
+        date = news.find("div", attrs={'class': 'hVLK8'}).text.strip()
+        locations = [ent.text for ent in nlp(body).ents if ent.label_ == "GPE" or ent.label_ == "LOC"]
+
+        data[i] = (date, title, body, locations)
+
+    # print(DataFrame(data, columns=['Date', 'Title', 'Body', 'Locations']))
+
+    return jsonify(data)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host="localhost")
